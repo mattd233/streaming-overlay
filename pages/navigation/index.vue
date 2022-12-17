8880287f9e6e30142fd3960ee0b573b84575e098
Matt <matthewsdeng@gmail.com>
2022-12-17 17:40:13 +1100

    Init

---
+++ pages/navigation/index.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="home">
+  <nav>
+    <NuxtLink to="/sc">Superchat</NuxtLink> |
+    <NuxtLink to="/sc-list">历史Superchat</NuxtLink> | 
+    <NuxtLink to="/diange">点歌</NuxtLink>
+  </nav>
+  <h1>点歌Superchat后台</h1>
+</div>
+</template>
