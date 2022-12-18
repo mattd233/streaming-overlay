7971895dd99ea6c81db9038d211348f7885f7c41
Matt <matthewsdeng@gmail.com>
2022-12-18 21:59:12 +1100

    Fix playlist component

--- components/Playlist.vue
+++ components/Playlist.vue
@@ -1,92 +1,69 @@
 <template>
   <div class="playlist-component">
-    <h1 class="">今日歌单</h1>
-    <p class="date">{{ todayDate() }}</p>
-    <div class="playlist-container">
-      <div class="song" v-for="song in playlist" v-bind:key="song.id">
-        <span>{{ song.title }}</span>
-      </div>
+    <div class="header">今日歌单</div>
+    <span class="date">{{ todayDate() }}</span>
+    <div class="song" v-for="song in playlist" v-bind:key="song.id">
+      <span>{{ song.title }}</span>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import moment from "moment";
-import DataService from "../service/DataService";
 
-export default {
-  data() {
-    return {
-      playlist: [],
-      timer: "",
-    };
-  },
-  props: {},
-  methods: {
-    todayDate() {
-      let m = moment();
-      return m.format("YYYY/M/D");
-    },
-    
-    loadTodayPlaylist() {
-      DataService.getAllPlaylist()
-        .then((response) => {
-          this.playlist = response.data;
-          console.log(this.playlist);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
 
-    cancelAutoUpdate() {  
-      clearInterval(this.timer);  
-    },  
-  },
-  mounted() {
-    this.loadTodayPlaylist();
-    // reload every 8 seconds
-    this.timer = setInterval(this.loadTodayPlaylist, 8000);
-  },
-  BeforeUnmount() {  
-    this.cancelAutoUpdate();  
-  },  
-};
+let playlist = ref([{
+  id: undefined,
+  title: undefined
+}]);
+let timer;
+
+// TODO change to today
+async function updatePlaylist() {
+  playlist.value = await fetch('http://localhost:3000/api/playlist/all').then(res => res.json());
+}
+
+const todayDate = () => {
+  let m = moment();
+  return m.format("YYYY/M/D");
+}
+
+onMounted(() => {
+  updatePlaylist();
+  timer = setInterval(async() => {
+    updatePlaylist();
+  }, 5000)
+})
 </script>
 
-<style scoped>
+<style lang="scss">
 .playlist-component {
-  margin: auto;
-  margin-top: 20px;
-  /* border-style: solid; */
-  padding: 25px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: black;
-}
-h1 {
-  font-size: 24pt;
-  text-align: center;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
-.date {
-  font-size: 14pt;
-  text-align: center;
-  margin-top: 0px;
-  margin-bottom: 15px;
-  font-weight: bold;
-}
-.song {
-  /* margin: 5px; */
-  font-size: 18pt;
   align-items: center;
-  height: 100%;
-  line-height: 35px;
-  height: 35px;
-  color: black;
-  font-weight: bold;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  width: 100vw;
+
+  >.header {
+    font-size: 25pt;
+    font-weight: bold;
+    padding-bottom: 10px;
+    text-align: center;
+  }
+
+  >.date {
+    font-size: 14pt;
+    font-weight: bold;
+    padding-bottom: 10px;
+    text-align: center;
+  }
+
+  >.song {
+    align-items: center;
+    font-size: 18pt;
+    font-weight: bold;
+    line-height: 35px;
+  }
 }
 </style>
