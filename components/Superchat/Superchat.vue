6530eb98569b28bcda948cc5ba89c40fd24634bc
Matt <matthewsdeng@gmail.com>
2023-05-10 09:51:48 +1000

    Send notification for new superchat

--- components/Superchat/Superchat.vue
+++ components/Superchat/Superchat.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="scContainer">
-    <div class="sc fadeInLeft" v-for="sc in superchats.slice(0, 1)" v-bind:key="sc.id">
-      <div class="upperBox" v-bind:style="calcTitleBg(sc.total_price)">
-        <img class="avatar" :src="`https://apic.douyucdn.cn/upload/${sc.avatar}_small.jpg`" alt="avatar"
-          loading="lazy" />
+    <div v-for="sc in superchats.slice(0, 1)" :key="sc.id" class="sc fadeInLeft">
+      <div class="upperBox" :style="calcTitleBg(sc.total_price)">
+        <img class="avatar" :src="`https://apic.douyucdn.cn/upload/${sc.avatar}_small.jpg`" alt="avatar" loading="lazy">
         <div class="upperBoxContent">
           <div class="nickname">
             {{ sc.belongs_to_user }}
           </div>
           <!-- convert to whole number -->
-          <div class="price">{{ sc.total_price ? `￥${Math.floor(sc.total_price)}` : '' }}</div>
+          <div class="price">
+            {{ sc.total_price ? `￥${Math.floor(sc.total_price)}` : '' }}
+          </div>
         </div>
-        <div class="timer" v-if="countdownString !== '00:00:00'">
+        <div v-if="countdownString !== '00:00:00'" class="timer">
           <Icon class="icon" name="bi:stopwatch" />
           &nbsp;{{ countdownString }}
         </div>
       </div>
-      <div class="bottomBox" v-bind:style="calcTextBg(sc.total_price)">
-        <p class="text">{{ sc.text ? sc.text : '' }}</p>
+      <div class="bottomBox" :style="calcTextBg(sc.total_price)">
+        <p class="text">
+          {{ sc.text ? sc.text : '' }}
+        </p>
       </div>
     </div>
   </div>
@@ -26,77 +29,86 @@
 <script lang="ts" setup>
 import { superchat } from '.prisma/client';
 
-import { background } from "./background";
-import { options } from "./options";
+import { background } from './background';
+import { options } from './options';
 
-let superchats = ref<any[]>([]);
-let countdownString = ref('');
+const superchats = ref<any[]>([]);
+const countdownString = ref('');
 let coundownTimer: NodeJS.Timer;
 let loadTimer: NodeJS.Timer;
 let updateTimer: NodeJS.Timer;
 
-async function loadSuperchats() {
-  const data = await (await fetch('http://localhost:3000/api/superchat/valid')).json()
+async function loadSuperchats(initialLoad = false) {
+  const data = await (await fetch('http://localhost:3000/api/superchat/valid')).json();
   // put all new sueprchats on top of the stack
   data.forEach((newSuperchat: superchat) => {
     if (superchats.value.every((old) => old.id !== newSuperchat.id)) {
       superchats.value.unshift(newSuperchat);
+      if (!initialLoad) {
+        showNotification(newSuperchat);
+      }
     }
   });
 }
 
-async function expireSuperchat(id: number) {
-  await fetch(`http://localhost:3000/api/superchat/expire/${id}`)
+function showNotification(superchat: superchat) {
+  const title = '你有一条新的sc';
+  const options = {
+    body: `${superchat.belongs_to_user} 发送了一条新的sc`
+  };
+
+  const notification = new Notification(title, options);
+  setTimeout(() => {
+    notification.close();
+  }, 5000);
 }
 
-async function updateSuperchats() {
+async function expireSuperchat(id: number) {
+  await fetch(`http://localhost:3000/api/superchat/expire/${id}`);
+}
+
+function updateSuperchats() {
   const head = superchats.value.shift();
   if (!head || head.id === undefined) {
     return;
   }
 
-  const total_price = head.total_price !== -1 ? head.total_price : undefined;
+  const totalPrice = head.total_price !== -1 ? head.total_price : undefined;
   const secondsElapsed = head.created_at ? (Date.now() - Date.parse(head.created_at.toString())) / 1000 : undefined;
-  if (!total_price || !secondsElapsed) {
+  if (!totalPrice || !secondsElapsed) {
     return;
-  }
-  else if (
-    total_price < options.sclv2Price &&
+  } else if (
+    totalPrice < options.sclv2Price &&
     secondsElapsed > options.sclv1LastingTime
   ) {
     expireSuperchat(head.id);
     return;
-  }
-  else if (
-    total_price < options.sclv3Price &&
+  } else if (
+    totalPrice < options.sclv3Price &&
     secondsElapsed > options.sclv2LastingTime
   ) {
     expireSuperchat(head.id);
     return;
-  }
-  else if (
-    total_price < options.sclv4Price &&
+  } else if (
+    totalPrice < options.sclv4Price &&
     secondsElapsed > options.sclv3LastingTime
   ) {
     expireSuperchat(head.id);
     return;
-  }
-  else if (
-    total_price < options.sclv5Price &&
+  } else if (
+    totalPrice < options.sclv5Price &&
     secondsElapsed > options.sclv4LastingTime
   ) {
     expireSuperchat(head.id);
     return;
-  }
-  else if (
-    total_price < options.sclv6Price &&
+  } else if (
+    totalPrice < options.sclv6Price &&
     secondsElapsed > options.sclv5LastingTime
   ) {
     expireSuperchat(head.id);
     return;
-  }
-  else if (
-    total_price >= options.sclv6Price &&
+  } else if (
+    totalPrice >= options.sclv6Price &&
     secondsElapsed > options.sclv6LastingTime
   ) {
     expireSuperchat(head.id);
@@ -110,23 +122,17 @@ async function updateSuperchats() {
 function calcTitleBg(price: number | null) {
   if (!price) {
     return background.sclv1TitleBg;
-  }
-  else if (price < options.sclv2Price) {
+  } else if (price < options.sclv2Price) {
     return background.sclv1TitleBg;
-  }
-  else if (price < options.sclv3Price) {
+  } else if (price < options.sclv3Price) {
     return background.sclv2TitleBg;
-  }
-  else if (price < options.sclv4Price) {
+  } else if (price < options.sclv4Price) {
     return background.sclv3TitleBg;
-  }
-  else if (price < options.sclv5Price) {
+  } else if (price < options.sclv5Price) {
     return background.sclv4TitleBg;
-  }
-  else if (price < options.sclv6Price) {
+  } else if (price < options.sclv6Price) {
     return background.sclv5TitleBg;
-  }
-  else if (price >= options.sclv6Price) {
+  } else if (price >= options.sclv6Price) {
     return background.sclv6TitleBg;
   }
 }
@@ -135,29 +141,22 @@ function calcTitleBg(price: number | null) {
 function calcTextBg(price: number | null) {
   if (!price) {
     return background.sclv1TextBg;
-  }
-  else if (price < options.sclv2Price) {
+  } else if (price < options.sclv2Price) {
     return background.sclv1TextBg;
-  }
-  else if (price < options.sclv3Price) {
+  } else if (price < options.sclv3Price) {
     return background.sclv2TextBg;
-  }
-  else if (price < options.sclv4Price) {
+  } else if (price < options.sclv4Price) {
     return background.sclv3TextBg;
-  }
-  else if (price < options.sclv5Price) {
+  } else if (price < options.sclv5Price) {
     return background.sclv4TextBg;
-  }
-  else if (price < options.sclv6Price) {
+  } else if (price < options.sclv6Price) {
     return background.sclv5TextBg;
-  }
-  else if (price >= options.sclv6Price) {
+  } else if (price >= options.sclv6Price) {
     return background.sclv6TextBg;
   }
 }
 
 function setCountdown() {
-  console.log('setCountdown')
   const head = superchats.value[0];
   const price = head?.total_price;
   const secondsElapsed = head?.created_at ? (Date.now() - Date.parse(head.created_at.toString())) / 1000 : undefined;
@@ -183,34 +182,35 @@ function setCountdown() {
   if (!secondsElapsed || !lastingTime) {
     return;
   }
-  const sec_num = lastingTime - secondsElapsed;
-  if (sec_num < 0) {
-    countdownString.value = "00:00:00";
-    return
+  const remainingSeconds = lastingTime - secondsElapsed;
+  if (remainingSeconds < 0) {
+    countdownString.value = '00:00:00';
+    return;
   }
-  const hours = Math.floor(sec_num / 3600) % 24;
-  const minutes = Math.floor(sec_num / 60) % 60;
-  const seconds = Math.floor(sec_num) % 60;
-  const hoursString = hours < 10 ? "0" + hours : hours.toString();
-  const minutesString = minutes < 10 ? "0" + minutes : minutes.toString();
-  const secondsString = seconds < 10 ? "0" + seconds : seconds.toString();
+  const hours = Math.floor(remainingSeconds / 3600) % 24;
+  const minutes = Math.floor(remainingSeconds / 60) % 60;
+  const seconds = Math.floor(remainingSeconds) % 60;
+  const hoursString = hours < 10 ? `0${hours}` : hours.toString();
+  const minutesString = minutes < 10 ? `0${minutes}` : minutes.toString();
+  const secondsString = seconds < 10 ? `0${seconds}` : seconds.toString();
   countdownString.value = `${hoursString}:${minutesString}:${secondsString}`;
 }
 
 onMounted(() => {
-  loadSuperchats();
+  Notification.requestPermission();
+  loadSuperchats(true);
   // load from backend every loadInterval seconds
   loadTimer = setInterval(loadSuperchats, options.loadInterval * 1000);
   // switch sc every switchInterval seconds
   updateTimer = setInterval(updateSuperchats, options.switchInterval * 1000);
   coundownTimer = setInterval(setCountdown, 1000);
-})
+});
 
 onBeforeUnmount(() => {
   clearInterval(loadTimer);
   clearInterval(updateTimer);
   clearInterval(coundownTimer);
-})
+});
 </script>
 
 <style lang="scss">
